ca8c4d3105ebd26cb191583262a3e23ae55cf410
Tomas Toledo <trtoledo1977@gmail.com>
2018-11-19 08:44:20 +0000

    migration

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,22 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    
+  end
+
+  get 'figures/:id' do
+    erb :'figure/show'
+  end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,14 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/lamdmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index.erb'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new.erb'
+  end
+
+  
 end
